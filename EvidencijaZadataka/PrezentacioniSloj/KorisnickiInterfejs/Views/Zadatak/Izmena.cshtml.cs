69ad54e9984ef0eb126cada1e8eb9eab58d7fcf9
Milovan <milovanjovanov03@gmail.com>
2026-09-19 22:04:34 +0200

    CRUD zavrsen
    dodata funkcija za izmenu zadataka

---
+++ EvidencijaZadataka/PrezentacioniSloj/KorisnickiInterfejs/Views/Zadatak/Izmena.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PrezentacioniSloj.KorisnickiInterfejs.Views.Zadatak
+{
+    public class IzmenaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
